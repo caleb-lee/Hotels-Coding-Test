a601d9c68e3b744dc205e8dd576d59177c51fe49
Caleb Lee <caleb.a.lee@icloud.com>
2015-04-21 13:48:08 -0500

    Defines and implements HotelManager class, which allows for reading data from a json, keeping a list of hotels, and sorting that list

---
+++ Hotels Coding Test/HotelManager.h
@@ -0,0 +1,35 @@
+//
+//  HotelManager.h
+//  Hotels Coding Test
+//
+//  Created by Caleb Lee on 2015/04/21.
+//  Copyright (c) 2015年 Caleb Lee. All rights reserved.
+//
+
+// This class manages the current list of hotels. It will read from the JSON to generate
+//  a list and sort the list however the user specifies.
+
+#import <Foundation/Foundation.h>
+#import "Hotel.h"
+
+typedef NS_ENUM(NSInteger, HotelManagerSortScheme) {
+    HotelManagerSortSchemeTotalRateAscending,
+    HotelManagerSortSchemeDistanceFromCurrentLocationAscending,
+    HotelManagerSortSchemeStarRatingDescending
+};
+
+@interface HotelManager : NSObject
+
+// this is a singleton class
++ (HotelManager*)sharedManager;
+
+// gets number of hotels in manager
+- (NSInteger)count;
+
+// gets hotel at index (required: 0 <= index < count)
+- (Hotel*)hotelAtIndex:(NSInteger)index;
+
+// sorts the list of hotels by the user-given scheme
+- (void)sortHotelsByScheme:(HotelManagerSortScheme)sortScheme;
+
+@end
---
+++ Hotels Coding Test/HotelManager.m
@@ -0,0 +1,112 @@
+//
+//  HotelManager.m
+//  Hotels Coding Test
+//
+//  Created by Caleb Lee on 2015/04/21.
+//  Copyright (c) 2015年 Caleb Lee. All rights reserved.
+//
+
+#import "HotelManager.h"
+
+@interface HotelManager ()
+
+@property (strong) NSMutableArray *hotels;
+
+@end
+
+@implementation HotelManager
+
+#define HOTEL_JSON_FILENAME @"hotels"
+#define HOTEL_JSON_KEY @"hotels"
+
+// this is a singleton class
++ (HotelManager*)sharedManager {
+    static HotelManager *sharedManager = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedManager = [[self alloc] init];
+    });
+    
+    return sharedManager;
+}
+
+// gets number of hotels in manager
+- (NSInteger)count {
+    return [_hotels count];
+}
+
+// gets hotel at index (required: 0 <= index < count)
+- (Hotel*)hotelAtIndex:(NSInteger)index {
+    if (index < 0 || index >= [self count])
+        [NSException raise:@"Out of Bounds Exception" format:@"hotelAtIndex: index %ld is out of bounds.", index];
+    
+    return (Hotel*)[_hotels objectAtIndex:index];
+}
+
+// sorts the list of hotels by the user-given scheme
+- (void)sortHotelsByScheme:(HotelManagerSortScheme)sortScheme {
+    // make first sort descriptor with the user-given sort scheme
+    NSSortDescriptor *variableSortDescriptor;
+    switch (sortScheme) {
+        case HotelManagerSortSchemeTotalRateAscending:
+            variableSortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"totalRate" ascending:YES];
+            break;
+        case HotelManagerSortSchemeDistanceFromCurrentLocationAscending:
+            variableSortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"distance" ascending:YES];
+            break;
+        case HotelManagerSortSchemeStarRatingDescending:
+            variableSortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"starRating" ascending:NO];
+    }
+    
+    // make second sort descriptor the hotel name
+    NSSortDescriptor *hotelNameSortDescriptor = [[NSSortDescriptor alloc] initWithKey:@"name" ascending:YES];
+    
+    // make a sorted array using sort descriptors
+    NSArray *sortDescriptors = @[variableSortDescriptor, hotelNameSortDescriptor];
+    NSArray *sortedArray = [_hotels sortedArrayUsingDescriptors:sortDescriptors];
+    
+    // set the class's array as the sorted array
+    _hotels = [[NSMutableArray alloc] initWithArray:sortedArray];
+}
+
+// when this class gets initialized, we want to generate the list of hotels
+- (id)init {
+    if (self = [super init]) {
+        [self generateHotelArray];
+    }
+    
+    return self;
+}
+
+// initializes the hotels array from json
+- (void)generateHotelArray {
+    // alloc/init array
+    _hotels = [[NSMutableArray alloc] init];
+    
+    // get array of hotel dictionaries
+    NSArray *hotelDictionaries = [self readJSONHotels];
+    
+    // fill hotels array with hotel objects created from the dictionary array
+    for (NSDictionary *hotelDictionary in hotelDictionaries) {
+        Hotel *hotel = [[Hotel alloc] initWithDictionary:hotelDictionary];
+        [_hotels addObject:hotel];
+    }
+    
+    // sorts array by lowest to highest total rate
+    [self sortHotelsByScheme:HotelManagerSortSchemeTotalRateAscending];
+}
+
+// parses JSON file, generates an array of dictionaries containing hotel data
+- (NSArray*)readJSONHotels {
+    NSData *jsonFileData = [NSData dataWithContentsOfFile:[[NSBundle mainBundle] pathForResource:HOTEL_JSON_FILENAME ofType:@"json"]];
+    NSError *error;
+    NSDictionary *jsonParsedData = [NSJSONSerialization JSONObjectWithData:jsonFileData options:kNilOptions error:&error];
+    NSArray *results = [jsonParsedData valueForKey:HOTEL_JSON_KEY];
+    
+    if (error)
+        NSLog(@"%@", error);
+    
+    return results;
+}
+
+@end
